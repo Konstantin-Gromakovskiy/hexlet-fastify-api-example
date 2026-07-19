8888545035941189673d7b5015f7bdd90a7f9c04
konstantin <k.hromakovskiy@yandex.ru>
2026-07-19 14:47:03 +0300

    Сгенерировал схему валидации через OpenApi

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-import path from 'node:path'
-import AutoLoad from '@fastify/autoload'
-import { fileURLToPath } from 'node:url'
+import path from "node:path";
+import AutoLoad from "@fastify/autoload";
+import { fileURLToPath } from "node:url";
+import { TypeBoxValidatorCompiler } from "@fastify/type-provider-typebox";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Pass --options via CLI arguments in command to enable these options.
-export const options = {}
+export const options = {};
 
 export default async function (fastify, opts) {
   // Place here your custom code!
 
+  const api = fastify.setValidatorCompiler(TypeBoxValidatorCompiler).withTypeProvider();
   // Do not touch the following lines
 
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
+  api.register(AutoLoad, {
+    dir: path.join(__dirname, "plugins"),
+    options: Object.assign({}, opts),
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
+  api.register(AutoLoad, {
+    dir: path.join(__dirname, "routes"),
+    options: Object.assign({}, opts),
+  });
 }
